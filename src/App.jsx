620f42c30935c3020d39569c830bdd84f11826b8
thecodejit <43138013+thecodejit@users.noreply.github.com>
2024-10-19 23:48:43 +0530

    Added Navbar

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from "./components"
+
+export default function App(){
+    return(
+    <>
+    <Navbar/>
+    </>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="">Projects</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Resources</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
